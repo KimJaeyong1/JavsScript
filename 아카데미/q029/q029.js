aafdb1301d3c67c705b0bec714c8d37fb1415239
ShenWoo <95727730+KimJaeyong1@users.noreply.github.com>
2021-12-16 21:51:18 +0900

    Add files via upload

--- 아카데미/q029/q029.js
+++ 아카데미/q029/q029.js
@@ -2,9 +2,9 @@
 var player = prompt("가위=1 바위=2 보=3")
 var com = Math.floor(Math.random()*3+1);
 
-var sc = 1; // 가위
-var ro = 2; // 바위
-var pa = 3; // 보
+// 1 = 가위
+// 2 = 바위
+// 3 = 보
 
 if ( (player==1 && com==3) || (player==2 && com ==1) || (player==3 && com==2) ) {
     dw ("플레이어 승리!");
@@ -16,5 +16,7 @@ if ( (player==1 && com==3) || (player==2 && com ==1) || (player==3 && com==2) ) 
 br();
 br();
 br();
-
 dw ('player (' + player +')' + ' : com  (' + com +')');
+br();
+br();
+dw ('| 1 = 가위 / 2 = 바위 / 3 = 보 |')
