8497dfbb18522a45880e8cd91fc3972918a34d9f
ShenWoo <95727730+KimJaeyong1@users.noreply.github.com>
2021-12-17 09:56:25 +0900

    Add files via upload

---
+++ 아카데미/q029/q029-1.js
@@ -0,0 +1,38 @@
+// // Rock Paper Scissors 1
+// var player = prompt("가위=1 바위=2 보=3")
+// var com = Math.floor(Math.random()*3+1);
+
+// // 1 = 가위
+// // 2 = 바위
+// // 3 = 보
+
+
+// if ( (player==1 && com==3) || (player==2 && com==1) || (player==3 && com==2) ) {
+//     dw("플레이어 승리!");
+//     } else if ( player == com) {
+//             dw("드로우!");
+//     } else {
+//         dw ("플레이어 패패!");
+//     }
+//     br();
+
+// dw ( "player : " + player + "com : " + com + "<br>");
+// dw ("| 1= 가위 | 2 = 바위 | 3 = 보 |")
+
+// Rock Paper Scissors 2
+
+var player = prompt("가위 바위 보")
+var com = Math.floor(Math.random()*3+1);
+
+
+if ( (player=="가위" && com==3) || (player=="바위" && com==1) || (player=="보" && com==2) ) {
+    dw("플레이어 승리!");
+    } else if ( (player=="가위" && com==1) || (player=="바위" && com==2) || (player=="보" && com==3) ) {
+            dw("드로우!");
+    } else {
+        dw ("플레이어 패패!");
+    }
+    br();
+
+
+dw ( "player : " + player + " | com : " + com + "<br>");
