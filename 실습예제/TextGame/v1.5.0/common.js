233d69942d5899ea19ff9e7591c39490b8392074
ShenWoo <95727730+KimJaeyong1@users.noreply.github.com>
2022-01-02 23:50:14 +0900

    Text Game Updating

---
+++ 실습예제/TextGame/v1.5.0/common.js
@@ -0,0 +1,37 @@
+// function dw(ex) {
+//     document.write(ex);
+// }
+
+// function dw(ex) {
+//     var screenString = screen.value;
+//     screenString = screenString + ex;
+//     screen.value = screenString;
+// }
+
+function tv(ex) {
+    var screenString = screenMessageBox.value;
+    screenString = screenString + ex;
+    screenMessageBox.value = screenString;
+}
+
+function ptv(ex) {
+    var screenString = screenPlayerBox.value;
+    // screenString = screenString + ex;
+    // screenPlayerBox.value = screenString;
+    screenPlayerBox.value = ex;
+}
+
+function mtv(ex) {
+    var screenString = screenMonsterBox.value;
+    // screenString = screenString + ex;
+    // screenMonsterBox.value = screenString;
+    screenMonsterBox.value = ex;
+}
+
+function br() {
+    document.write("<br>");
+}
+
+function hr() {
+    document.write("<hr>");
+}
